e011cb5c44a35102bcf857c024601b9547569f56
Kim Lund Johansen <klj@delegate.dk>
2026-05-07 15:41:16 +0200

    chore: add bar and RecipeDetail

--- Cocktail.Web/Program.cs
+++ Cocktail.Web/Program.cs
@@ -1,28 +1,38 @@
 using Cocktail.Web.Components;
+using Cocktail.Web.Data;
+using Cocktail.Web.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+var dbPath = Path.Combine(builder.Environment.ContentRootPath, "cocktail.db");
+builder.Services.AddDbContextFactory<CocktailDb>(opt =>
+    opt.UseSqlite($"Data Source={dbPath}"));
+
+builder.Services.AddScoped<RecipeService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-
 app.UseAntiforgery();
-
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+using (var scope = app.Services.CreateScope())
+{
+    var factory = scope.ServiceProvider.GetRequiredService<IDbContextFactory<CocktailDb>>();
+    using var db = factory.CreateDbContext();
+    Seed.EnsureSeeded(db);
+}
 
 app.Run();
